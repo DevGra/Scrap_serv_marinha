73532961bf35a41a7e7c5b230644e11cd7138d77
Carlos Graziani de Toledo <CGT@fapesp.local>
2019-11-01 15:36:46 -0300

    ajuste da escrita do cabecalho do excel

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ import csv
 base_url = 'http://www.portaltransparencia.gov.br/servidores/consulta?paginacaoSimples=true&tamanhoPagina=&offset=&direcaoOrdenacao=asc&colunasSelecionadas=detalhar%2Ctipo%2Ccpf%2Cnome%2CorgaoServidorExercicio%2CorgaoServidorLotacao%2Cmatricula%2CtipoVinculo%2Cfuncao&orgaosServidorExercicio=OR70000&tipo=2&ordenarPor=nome&direcao=asc'
 
 pg = requests.get(base_url)
-#import pdb; pdb.set_trace()
 
-# -------------------- montagem do link do ajax ---------------
+# -------------------- montagem do link do ajax na funcao ---------------
 def ajax_principal(offset_size):
     # -- para a paginacao o offset varia de acordo com a qtde de itens por pg
     # -- comeca em 0 e se a qtde de itens exibidos for de 50 por pg, o offset
@@ -50,61 +49,45 @@ def ajax_principal(offset_size):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36",
             "X-Requested-With": "XMLHttpRequest"
         }
-    #import pdb; pdb.set_trace()
+
     ajax_response = requests.get(ajax_url, headers=customHead, data=data_ajax)
-    #import pdb; pdb.set_trace()
     dict_convenio = json.loads(ajax_response.content)
-    #import pdb; pdb.set_trace()
     return dict_convenio
     # --------------------   fim da funcao ajax principal -----------
-
-# teste = ajax_principal(offset)
-# dados = teste['data']
-# import pdb; pdb.set_trace()
-# print("Fim")
-# exit()
 
 i = 1
 offset_size = 0
 l = 1 # somente para controle de quantidades de pagina
-#import pdb; pdb.set_trace()
 while True:
-    nome_arquivo = 'testeservidores.csv'
+    nome_arquivo = 'teste.csv'
     ajax_pg_main = ajax_principal(offset_size)
-    #import pdb; pdb.set_trace()
     if ajax_pg_main['data']:
         data = ajax_pg_main['data']
         colunas = dict(data[0])
         nm_columns = ''
-        #import pdb; pdb.set_trace()
         # try:
-        with open(nome_arquivo, mode = 'a', encoding = 'utf-8') as f:
-            if  i == 1:
-                # pega o nome das colunas em data
-                nm_columns = colunas.keys()
-                nm_columns = list(nm_columns)
-                import pdb; pdb.set_trace()
-                writer = csv.DictWriter(f, fieldnames=nm_columns) # lineterminator = '\n'
-                writer.writeheader()
-                print("FIM DA GRAVACAO DO CABEÇALHO DE SERVIDORES")
+        if  i == 1:
+            with open(nome_arquivo, mode = 'w', encoding = 'utf-8') as f:
 
-            for registro in data:
-
-                print(l)
-                print(registro)
-                # --------- VALORES DETALHADOS DE CADA CONVENIO ---------
-                valores = registro.values()
-                valor = list(valores)
-                #import pdb; pdb.set_trace()
-                with open(nome_arquivo, 'a') as csvfile:
-                    writer = csv.writer(csvfile)
-                    writer.writerow(valor)
-                    #csvfile.close()
-                #valores.update({'ValorTotal': valor_total, 'Valor_2018': valores })
-                #writer.writerow(valor)
-                print("FIM DA GRAVACAO DA LINHA DE CONVENIO")
-                l += 1
-    # verifica se a primera pagina exibe 50 itens, se sim, continua o loop.
+                    # pega o nome das colunas em data
+                    nm_columns = colunas.keys()
+                    nm_columns = list(nm_columns)
+                    writer = csv.DictWriter(f, fieldnames=nm_columns) # lineterminator = '\n'
+                    writer.writeheader()
+                    print("FIM DA GRAVACAO DO CABEÇALHO DE SERVIDORES")
+        import pdb; pdb.set_trace()
+        for registro in data:
+            print(l)
+            print(registro)
+            # --------- VALORES DETALHADOS DE CADA CONVENIO ---------
+            valores = registro.values()
+            valor = list(valores)
+            with open(nome_arquivo, 'a') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(valor)
+            print("FIM DA GRAVACAO DA LINHA DE CONVENIO")
+            l += 1
+    # verifica se a pagina exibe 50 itens, se sim, continua o loop.
     if len(ajax_pg_main['data']) == 50:
         i += 1
         # pq eh 50 por pagina
